dc3de74602986632843029bbbab85ba77735a6a7
viateur <akimanaviateur94@gmail.com>
2024-06-03 16:54:19 +0200

    handlingreal time ambulance location

--- Mediconnect_frontend-main/android/app/src/main/kotlin/com/example/mediconnect/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mediconnect
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- routes/ambulanceRoutes.js
+++ routes/ambulanceRoutes.js
@@ -1,13 +1,49 @@
-const express = require('express');
+const bodyParser = require("body-parser");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
+const express = require("express");
 const router = express.Router();
-const ambulanceController = require('../controllers/ambulanceController');
-const authMiddleware = require('../middleware/authMiddleware');
+const ambulanceController = require("../controllers/ambulanceController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.use(authMiddleware);
 
-router.get('/ambulances', ambulanceController.getAmbulances);
-router.post('/ambulances', ambulanceController.createAmbulance);
-router.put('/ambulances/:id', ambulanceController.updateAmbulance);
-router.delete('/ambulances/:id', ambulanceController.deleteAmbulance);
+router.get("/ambulances", ambulanceController.getAmbulances);
+router.post("/ambulances", ambulanceController.createAmbulance);
+router.put("/ambulances/:id", ambulanceController.updateAmbulance);
+router.delete("/ambulances/:id", ambulanceController.deleteAmbulance);
+io.on("connection", (socket) => {
+  logger.info("New client connected");
+
+  socket.on("updateLocation", async (data) => {
+    try {
+      const ambulance = await Ambulance.findByIdAndUpdate(
+        data.id,
+        {
+          location: {
+            type: "Point",
+            coordinates: [data.longitude, data.latitude],
+          },
+        },
+        { new: true }
+      );
+      io.emit("locationUpdate", ambulance);
+    } catch (err) {
+      logger.error("Error updating location:", err);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    logger.info("Client disconnected");
+  });
+});
 
 module.exports = router;
