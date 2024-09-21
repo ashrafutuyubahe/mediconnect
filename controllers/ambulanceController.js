650d8d180184999735441b90d85f6a99f129ef96
Ashrafu <ashraftuyubahe001@gmail.com>
2024-09-21 11:30:41 +0200

    finished and tested the ambulance enpoints

--- controllers/ambulanceController.js
+++ controllers/ambulanceController.js
@@ -14,6 +14,14 @@ exports.getAmbulances = async (req, res) => {
 exports.createAmbulance = async (req, res) => {
   try {
     const { licensePlate, make, model, latitude, longitude } = req.body;
+    if(!licensePlate||!make||!model||!latitude|| !longitude){
+       return res.status(401).send("please all fields are  required");
+    }
+    const findAmbulanceExists= await Ambulance.findOne({licensePlate});
+    if(findAmbulanceExists){
+      res.status(401).send("the ambulance already exists");
+    }
+
     const newAmbulance = new Ambulance({
       licensePlate,
       make,
@@ -23,8 +31,11 @@ exports.createAmbulance = async (req, res) => {
         coordinates: [longitude, latitude],
       },
     });
-    await newAmbulance.save();
-    res.status(201).json(newAmbulance);
+      if(await newAmbulance.save()){
+        return res.status(201).json({message:"ambulance is created",newAmbulance});
+      }
+
+      return res.status(401).send("failed to create ambulance");
   } catch (err) {
     logger.error("Error creating ambulance:", err);
     res.status(500).json({ error: "Internal server error" });
@@ -35,6 +46,17 @@ exports.updateAmbulance = async (req, res) => {
   try {
     const { id } = req.params;
     const { licensePlate, make, model, latitude, longitude } = req.body;
+
+    if(!licensePlate||!make||!model||!latitude|| !longitude){
+      return res.status(401).send("please all fields are  required");
+   }
+   
+    const findAmbulanceExists= await Ambulance.findById(id);
+
+    if(!findAmbulanceExists){
+    return res.status(404).json({ error: "Ambulance not found" });
+    }
+
     const updatedAmbulance = await Ambulance.findByIdAndUpdate(
       id,
       {
@@ -49,9 +71,10 @@ exports.updateAmbulance = async (req, res) => {
       { new: true }
     );
     if (!updatedAmbulance) {
-      return res.status(404).json({ error: "Ambulance not found" });
+      return res.status(404).json({ error: "failed to update the Ambulance" });
     }
-    res.json(updatedAmbulance);
+
+    res.json({message:"the ambulance has been updated",updatedAmbulance});
   } catch (err) {
     logger.error("Error updating ambulance:", err);
     res.status(500).json({ error: "Internal server error" });
@@ -61,10 +84,17 @@ exports.updateAmbulance = async (req, res) => {
 exports.deleteAmbulance = async (req, res) => {
   try {
     const { id } = req.params;
-    const deletedAmbulance = await Ambulance.findByIdAndDelete(id);
-    if (!deletedAmbulance) {
-      return res.status(404).json({ error: "Ambulance not found" });
+    
+    const findAmbulanceExists= await Ambulance.findById(id);
+    if(!findAmbulanceExists){
+    return res.status(404).json({ error: "Ambulance not found" });
     }
+    const deletedAmbulance = await Ambulance.findByIdAndDelete(id);
+
+    if (!deletedAmbulance) {
+      return res.status(404).json({ error: " failed to delete ambulance" });
+    }
+
     res.json({ message: "Ambulance deleted" });
   } catch (err) {
     logger.error("Error deleting ambulance:", err);
